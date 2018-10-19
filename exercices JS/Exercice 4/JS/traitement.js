f5107069d710bd0fe78c87b54db3c288638ae4f3
TheCode-GOD <SAVSI1431411@etu.cegepjonquiere.ca>
2018-10-19 11:05:30 -0400

    Exercice JS 1-6

---
+++ exercices JS/Exercice 4/JS/traitement.js
@@ -0,0 +1,14 @@
+function btnCalculer_onclick()
+{ 
+ 	var Destination, Distance, Consomation, Essence, Cout
+
+    Destination = document.getElementById("Destination").value;
+ 	Distance = parseFloat(document.getElementById("Distance").value);
+ 	Consomation = parseFloat(document.getElementById("Consomation").value);
+ 	Essence = parseFloat(document.getElementById("Essence").value);
+
+ 	Cout = ((Distance/100)*Consomation*Essence);
+
+    console.log(Cout.toFixed(2)+" $"+" pour aller à "+Destination);
+
+}
