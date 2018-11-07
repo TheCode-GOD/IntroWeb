627cd9fc9816e58cfe251df11808b80267d504b0
TheCode-GOD <SAVSI1431411@etu.cegepjonquiere.ca>
2018-11-07 10:54:58 -0500

    lab institut final

--- exercices JS/GarderieAnimaux/JS/traitement.js
+++ exercices JS/GarderieAnimaux/JS/traitement.js
@@ -1,12 +1,20 @@
 function btnCalculer_onclick()
 { 
- 	var Veterinaire, NbreJours, Serv, animaux, prixParJour, totPrix, Prix, Final
+ 	var Veterinaire, NbreJours, Serv, animaux, prixParJour, totPrix, Prix, Final, Montant
 
     Veterinaire = document.getElementById("lstVeterinaire").value;
  	NbreJours = parseInt(document.getElementById("txtNbreJours").value);
 
+ 	Montant=0;
 
- 	if (document.getElementById("chkServ").checked == true)
+    toilettage();
+    animal();
+    nbreMontant();
+    montantveterinaire();
+
+function toilettage()
+{
+    if (document.getElementById("chkServ").checked == true)
     {
         Serv = "avec service de toilettage ";
         Prix=5;
@@ -16,24 +24,69 @@ function btnCalculer_onclick()
         Serv = "sans service de toilettage ";
         Prix=0;
     }
-     if (document.getElementById("radChat").checked == true)
-     {
-         animaux = "chat ";
-         prixParJour = 17;
+}
+function animal()
+{
+    if (document.getElementById("radChat").checked == true)
+    {
+        animaux = "chat ";
+        prixParJour = 17;
 
-     }
-     else
-     {
-         animaux = "chien ";
-         prixParJour = 16;
+    }
+    else
+    {
+        animaux = "chien ";
+        prixParJour = 16;
 
-     }
+    }
+}
+  function nbreMontant() {
+
+  }
+
+    if (NbreJours < 5 && NbreJours > 10)
+    {
+        Montant = Montant*0.95;
+    }
+    else if (NbreJours < 10 && NbreJours > 30)
+    {
+        Montant = Montant*0.9;
+    }
+    else if (NbreJours > 30)
+    {
+        Montant =  Montant*0.85;
+    }
+function montantveterinaire() {
+    switch (Veterinaire)
+    {
+        case"Audrey Bouchard":
+            Montant = Montant+(NbreJours*25);
+            break;
+        case"Stéphane Tremblay":
+            Montant = Montant+(NbreJours*35);
+            break;
+        case"Maxime Simard":
+            Montant = Montant+(NbreJours*40);
+            break;
+        case"Mélissa Caron":
+            Montant = Montant+(NbreJours*45);
+            break;
+    }
+}
+
+
+
+
+
+
 
      //16.25 chien / 19.50 chat
 
     totPrix = (NbreJours*prixParJour)+Prix;
 
-    console.log("Vétérinaire responsable est " + Veterinaire + ". Le montant pour la garde de votre " + animaux + " est de " + totPrix + "$ pour " + NbreJours + " jour(s)," + Serv);
+    document.getElementById("lblMessage").innerHTML = "Vétérinaire responsable est " + Veterinaire + ". Le montant pour la garde de votre " + animaux + " est de " + totPrix + "$ pour " + NbreJours + " jour(s)," + Serv+" / un montant suplémentaire de "+Montant+"$  devra etre pour le vétérinaire";
+
+
 
 
 
